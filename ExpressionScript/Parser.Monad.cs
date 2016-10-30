7d61b1563610a04729eb283e3f296de735707068
glopes <glopesdev@users.noreply.github.com>
2016-10-30 16:44:27 +0000

    Added primitive parsers.

---
+++ ExpressionScript/Parser.Monad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionScript
+{
+    public static partial class Parser
+    {
+        public static Parser<TValue> Return<TValue>(TValue value)
+        {
+            return input => EnumerableEx.Return(new Result<TValue>(value, input));
+        }
+
+        public static Parser<TValue> Empty<TValue>()
+        {
+            return input => Enumerable.Empty<IResult<TValue>>();
+        }
+
+        public static Parser<char> Char()
+        {
+            return input => input.Take(1)
+                                 .Select(x => new Result<char>(x, input.Substring(1)));
+        }
+    }
+}
