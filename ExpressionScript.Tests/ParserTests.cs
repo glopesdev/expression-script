8a272ae0c205670052b5ba437645317a12567bd5
glopes <glopesdev@users.noreply.github.com>
2016-10-30 14:30:58 +0000

    Created ExpressionScript repository.

---
+++ ExpressionScript.Tests/ParserTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionScript.Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ExpressionScript
+
+ExpressionScript is a free and open-source programming language to enable scripting .NET Expression Trees from text in any application.
