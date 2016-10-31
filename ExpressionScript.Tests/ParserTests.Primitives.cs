ec362f319e63874b5018f69fb139132e45c5f866
glopes <glopesdev@users.noreply.github.com>
2016-10-31 12:39:46 +0000

    Added first unit test example.

---
+++ ExpressionScript.Tests/ParserTests.Primitives.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionScript.Tests
+{
+    [TestClass]
+    public partial class ParserTests
+    {
+        const string CharInput = "abFb67";
+
+        [TestMethod]
+        public void TestChar()
+        {
+            var parser = Parser.Char();
+            var results = parser(CharInput).ToArray();
+            Assert.AreEqual(1, results.Length);
+            Assert.AreEqual(CharInput[0], results[0].Value);
+        }
+    }
+}
